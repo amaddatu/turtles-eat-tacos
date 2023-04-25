3841d2ed1171094e58ebddc1d5f38a120d543f19
amaddatu <>
2023-04-25 19:02:44 -0400

    Add mvp code to test deployment

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// $('button').on('click')
+var formEl = document.querySelector('#search-form');
+var resultsEl = document.querySelector('#results-container');
+var inputEl = document.querySelector('[name=search]');
+formEl.addEventListener('submit', function(event){
+  event.preventDefault();
+  resultsEl.textContent="working??? " + inputEl.value;
+});
